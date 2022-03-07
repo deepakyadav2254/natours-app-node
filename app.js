48d58420102599a7763db864a11e2e792b1eaaa4
deepak yadav <deepak.y3031@gmail.com>
2022-03-07 15:14:54 -0400

    Add first route handler

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    data: tours,
+  });
+});
+
 console.log(tours);
 app.listen(port, () => {
   console.log('application running on 3000');
