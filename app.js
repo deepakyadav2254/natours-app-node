7c28b9c5f3a38dc9dc9abff0edfb767a78c76b3b
deepak yadav <deepak.y3031@gmail.com>
2022-03-07 16:41:03 -0400

    Add the first post route handler.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const fs = require('fs');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -17,7 +19,25 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-console.log(tours);
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log('application running on 3000');
 });
