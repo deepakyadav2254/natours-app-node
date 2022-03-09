2481dc012d3032c55b3b263dbf3cf1145d0e6c9c
deepak yadav <deepak.y3031@gmail.com>
2022-03-08 22:38:26 -0400

    Create server.js and seperate out server configurations.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const userRouter = require('./routes/userRouter');
 const tourRouter = require('./routes/tourRouter');
 const morgan = require('morgan');
-const port = 5000;
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -11,6 +10,4 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.listen(port, () => {
-  console.log('application running on 3000');
-});
+module.exports = app;
