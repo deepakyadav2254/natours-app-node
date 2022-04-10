a2f0b2efabefaafde5184d868f2a90561e8c30ce
deepak yadav <deepak.y3031@gmail.com>
2022-04-10 12:44:58 -0300

    Add dotenv to read and use variables from config.env file.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv'); // this is needed to pass all the variables defined in config.env to node.
 const app = require('./app');
 const port = 3000;
+
+dotenv.config({ path: './config.env' }); // tell the path of configuration file.
 
 /* console.log(process.env); process.env we can see all the node global variables. */
 app.listen(port, () => {
