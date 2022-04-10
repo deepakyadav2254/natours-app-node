82009d2ee411e23b7ea6612eab51549b5654843e
deepak yadav <deepak.y3031@gmail.com>
2022-04-10 12:58:58 -0300

    Use port ftom the config file.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const dotenv = require('dotenv'); // this is needed to pass all the variables defined in config.env to node.
 const app = require('./app');
-const port = 3000;
 
 dotenv.config({ path: './config.env' }); // tell the path of configuration file.
+const port = process.env.PORT || 3000;
 
 /* console.log(process.env); process.env we can see all the node global variables. */
+
 app.listen(port, () => {
   console.log('application running on 3000');
 });
