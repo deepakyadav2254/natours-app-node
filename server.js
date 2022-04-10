27f17669e57c1a2203a27adbd1afaa6292c0d0e3
deepak yadav <deepak.y3031@gmail.com>
2022-04-10 13:06:36 -0300

    Reposition variables in server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv'); // this is needed to pass all the variables defined in config.env to node.
-const app = require('./app');
-
 dotenv.config({ path: './config.env' }); // tell the path of configuration file.
+const app = require('./app');
 const port = process.env.PORT || 3000;
 
 /* console.log(process.env); process.env we can see all the node global variables. */
