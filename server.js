003730477217a6a9b9a7c9af058575651aa4afa2
deepak yadav <deepak.y3031@gmail.com>
2022-04-10 12:40:38 -0300

    Add comment regarding Node

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const port = 3000;
 
+/* console.log(process.env); process.env we can see all the node global variables. */
 app.listen(port, () => {
   console.log('application running on 3000');
 });
