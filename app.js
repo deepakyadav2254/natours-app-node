4f992d3f7f762998fa74bf6442ea0440c9dd78d6
deepak yadav <deepak.y3031@gmail.com>
2022-04-09 21:28:05 -0300

    Add code to serve static files.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = 5000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log('application running on 3000');
